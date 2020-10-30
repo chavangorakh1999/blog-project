cdb7ce5675061462fdf77a6cc325efbd35915d65
chavangorakh1999 <chavangorakh19999@ggmail.com>
2020-10-31 00:19:56 +0530

    adding procfiles and gitignore

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const mongoose = require("mongoose");
+
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -12,8 +14,31 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-var posts=[];
 
+mongoose.connect("mongodb+srv://grc_sr:Western@12@grcs-developers-club.g7k6t.mongodb.net/postsDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const postSchema = new mongoose.Schema({
+  postHeading: String,
+  postBody: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+  postHeading: "Day1",
+  postBody:
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+});
+const post2 = new Post({
+  postHeading: "Day2",
+  postBody:
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+});
+
+const defaultPost = [post1,post2];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,30 +46,49 @@ app.use(express.static("public"));
 
 let port = process.env.PORT || 3000;
 
-app.get("/",(req,res)=> {
-  res.render("home",{homeParagraph:homeStartingContent,newPost:posts});
+app.get("/", (req, res) => {
+  Post.find({}, (err,foundPosts) => {
+   
+      res.render("home", {
+        homeParagraph: homeStartingContent,
+        newPost: foundPosts,
+      });
+    });
+  
 });
 
-app.get("/about",(req,res) =>{
-  res.render("about",{aboutParagraph:aboutContent});
+app.get("/about", (req, res) => {
+  res.render("about", { aboutParagraph: aboutContent });
 });
 
-app.get("/contact",(req,res)=>{
-  res.render("contact",{contactParagraph:contactContent});
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactParagraph: contactContent });
 });
 
-app.get("/compose",(req,res) => {
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose",(req,res) => {
+app.post("/compose", (req, res) => {
+  const title = req.body.postTitle;
+  const body = req.body.postBody;
 
-  const post = {
-     title:req.body.postTitle,
-      body:req.body.postBody
-  };
-  posts.push(post);
+  const post = new Post({
+    postHeading: title,
+    postBody: body,
+  });
+  post.save(post);
   res.redirect("/");
+});
+app.get("/posts/:postId", (req, res) => {
+ const requestedId = req.params.postId;
+  Post.findOne({_id: requestedId}, (err, foundPost) => {
+    
+      res.render("post", {
+        title: foundPost.postHeading,
+        body: foundPost.postBody,
+      });
+  });
 });
 
 app.listen(port, () => {
